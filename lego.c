99fb31d67675564256d6bac4bd0f2df7a0a61c7b
ugoleone <ugo.cavalcanti@fastwebnet.it>
2017-05-03 20:32:11 +0200

    Prima versione stabile del sistema di guida
    Sistema di guida a 2 livelli di velocità e 3 livelli di curvatura.
    Lampeggianti
    Fari
    Funzione "Stop" (reset)

--- lego.c
+++ lego.c
@@ -1,21 +1,32 @@
 #include <ncurses.h>			/* ncurses.h includes stdio.h */
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include <wiringPi.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <wiringPi.h>
+#include <softPwm.h>
 
+const int DE = 2;
+const int DA = 3;
+const int DB = 8;
 
+const int SE = 5;
+const int SA = 6;
+const int SB = 4;
 
-const int DE = 1;
-const int DA = 2;
-const int DB = 3;
+const int LED1 = 9;
+const int LED2 = 10;
 
-const int SE = 4;
-const int SA = 5;
-const int SB = 6;
+const int LED3 = 11;
+const int LED4 = 12;
+const int LED5 = 13;
 
-int speed;  //valori ammessi: -2, -1, 0, 1, 2
-int turn;   //valori ammessi: -2, -1, 0, 1, 2
+int speed;  //valori ammessi: -1, 0, 1
+int turn;   //valori ammessi: -1, 0, 1
+int led;    //valori ammessi: 0, 1
+int fari;   //valori ammessi: 0, 1
+
+int dPWM, sPWM; //valori ammessi: -100, -50, 0, 50, 100. Dopo il calcolo del verso se ne prendono i moduli
 
 
 
@@ -77,15 +88,17 @@ void pwmINFO(int pin, int PWM){
 }
 
 
-////////////////////////////////////////////////PINOUT COMUNICATION///////////////////////////////////////////////
+////////////////////////////////////////////////////*** END ***///////////////////////////////////////////////////
 
+
+
+
+////////////////////////////////////////////PRIMO LIVELLO DI CONTROLLO////////////////////////////////////////////
 
 void accelera(){
+    turn = 0;
+    
     switch (speed) {
-        case -2:
-            speed = -1;
-            break;
-            
         case -1:
             speed = 0;
             break;
@@ -95,11 +108,7 @@ void accelera(){
             break;
             
         case 1:
-            speed = 2;
-            break;
-            
-        case 2:
-            speed = 2;
+            speed = 1;
             break;
             
         default:
@@ -108,13 +117,11 @@ void accelera(){
 }
 
 void rallenta(){
+    turn = 0;
+    
     switch (speed) {
-        case -2:
-            speed = -2;
-            break;
-            
         case -1:
-            speed = -2;
+            speed = -1;
             break;
             
         case 0:
@@ -125,20 +132,20 @@ void rallenta(){
             speed = 0;
             break;
             
-        case 2:
-            speed = 1;
-            break;
-            
         default:
             break;
     }
 }
 
+void brake(){
+    //brake resetta anche lo sterzo, piuttosto che frenare e basta
+    
+    speed = 0;
+    turn = 0;
+}
+
 void giraDx(){
     switch (turn) {
-        case -2:
-            turn = -1;
-            break;
             
         case -1:
             turn = 0;
@@ -149,11 +156,7 @@ void giraDx(){
             break;
             
         case 1:
-            turn = 2;
-            break;
-            
-        case 2:
-            turn = 2;
+            turn = 1;
             break;
             
         default:
@@ -164,12 +167,9 @@ void giraDx(){
 
 void giraSx(){
     switch (turn) {
-        case -2:
-            turn = -2;
-            break;
-            
+      
         case -1:
-            turn = -2;
+            turn = -1;
             break;
             
         case 0:
@@ -180,118 +180,184 @@ void giraSx(){
             turn = 0;
             break;
             
-        case 2:
-            turn = 1;
-            break;
-            
         default:
             break;
     }
 }
 
+////////////////////////////////////////////////////*** END ***///////////////////////////////////////////////////
 
-int setPWM(int vel){
-    //Ci sono 3 livelli di velocità: 0, 512, 1023
-    
-    if (vel == 2 || vel == -2)
-        return 1023;
-    
-    else if (vel == 1 || vel == -1)
-        return 512;
-    
-    else
-        return 0;
-}
 
-void setVerso(int vel){
+
+
+////////////////////////////////////////////SECONDO LIVELLO DI CONTROLLO//////////////////////////////////////////
+
+void initVerso(){
     
-    if (vel == 1 || vel == 2){
-        /*digitalWrite(DA, HIGH);
+    if (dPWM >= 0){
+        digitalWrite(DA, HIGH);
         digitalWrite(DB, LOW);
-        
-        digitalWrite(SA, HIGH);
-        digitalWrite(SB, LOW);*/
         
         digitalINFO(DA, C_HIGH);
         digitalINFO(DB, C_LOW);
+    }
+    else {
+        dPWM = -1 * dPWM;
+        
+        digitalWrite(DA, LOW);
+        digitalWrite(DB, HIGH);
+        
+        digitalINFO(DA, C_LOW);
+        digitalINFO(DB, C_HIGH);
+    }
+    
+    if (sPWM >= 0){
+        digitalWrite(SA, HIGH);
+        digitalWrite(SB, LOW);
         
         digitalINFO(SA, C_HIGH);
         digitalINFO(SB, C_LOW);
     }
-    
-    else if (vel == -1 || vel == -2){
-        /*digitalWrite(DA, LOW);
-        digitalWrite(DB, HIGH);
+    else {
+        sPWM = -1 * sPWM;
         
         digitalWrite(SA, LOW);
-        digitalWrite(SB, HIGH);*/
-        
-        digitalINFO(DA, C_LOW);
-        digitalINFO(DB, C_HIGH);
+        digitalWrite(SB, HIGH);
         
         digitalINFO(SA, C_LOW);
         digitalINFO(SB, C_HIGH);
     }
-    
 }
 
+void setMotori(){
+    
+    if (speed == 0) {
+        switch (turn) {
+            case 0:
+                dPWM = 0;
+                sPWM = 0;
+                break;
+                
+            case -1:
+                dPWM = 100;
+                sPWM = -100;
+                break;
+                
+            case 1:
+                dPWM = -100;
+                sPWM = 100;
+                break;
+                
+            default:
+                break;
+        }
 
+    }
+    else {
+        switch (turn) {
+            case 0:
+                dPWM = speed*100;
+                sPWM = speed*100;
+                break;
+                
+            case -1:
+                dPWM = speed*100;
+                sPWM = 0;
+                break;
+                
+            case 1:
+                dPWM = 0;
+                sPWM = speed*100;
+                break;
+                
+            default:
+                break;
+        }
+        
+    }
+    
+    //initVerso() trasforma i valori PWM da negativi a positivi, cambiando il verso dei motori
+    
+    initVerso();
 
-void aggiornaPWM(int sterzo, int vel){
-    
-    setVerso(vel);
-    
-    int differenza = 0, dPWM, sPWM;
-    
-    dPWM = setPWM(vel);
-    sPWM = dPWM;
-    
-    if (sterzo == 2 || sterzo == -2)
-        differenza = 500;
-    
-    else if (sterzo == 1 || sterzo == -1)
-        differenza = 250;
-    
-    else if (sterzo == 0)
-        differenza = 0;
-    
-    if (sterzo == 1 || sterzo == 2)
-        sPWM = dPWM - differenza;
-    
-    if (sterzo == -1 || sterzo == -2)
-        dPWM = sPWM - differenza;
-    
-    //gestione semplificata valori PWM negativi, si perde la possibilità di girare se speed = 0
-    
-    if (dPWM < 0)
-        dPWM = 0;
-    
-    if (sPWM < 0)
-        sPWM = 0;
-    
-    
-    /*pwmWrite(DE, dPWM);
-    pwmWrite(SE, sPWM);*/
+    softPwmWrite (DE, dPWM);
+    softPwmWrite (SE, sPWM);
     
     pwmINFO(DE, dPWM);
     pwmINFO(SE, sPWM);
     
 }
 
+////////////////////////////////////////////////////*** END ***///////////////////////////////////////////////////
 
-void brake(){
-    //brake resetta anche lo sterzo, piuttosto che frenare e basta
+
+
+
+
+//////////////////////////////////////////////////GESTIONE LUCI///////////////////////////////////////////////////
+
+void setFARI(){
+    if (fari == 0){
+        fari = 1;
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        digitalWrite(LED5, HIGH);
+    }
+    else{
+        fari = 0;
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+        digitalWrite(LED5, LOW);
+    }
+}
+
+void ledswitch(){
+    led = ((led) ? 0 : 1);
+}
+
+void ledHandler(int signo){
+    ledswitch();
+}
+
+void codiceFiglio(){
     
-    speed = 0;
-    turn = 0;
+    signal(SIGUSR1, ledHandler);
+    while (1) {
+        while (led) {
+            digitalWrite(LED1, HIGH);
+            digitalWrite(LED2, LOW);
+            sleep(1);
+            digitalWrite(LED1, LOW);
+            digitalWrite(LED2, HIGH);
+            sleep(1);
+        }
+        
+        if (!led){
+            digitalWrite(LED1, LOW);
+            digitalWrite(LED2, LOW);
+        }
+    }
 }
 
 
+
+
+////////////////////////////////////////////////////*** END ***///////////////////////////////////////////////////
+
+
+
+
+
+
 void printer(){
-    move(10,1);
+    move(9,1);
     printw("Turn: %d   ", turn);
-    move(11,1);
+    move(10,1);
     printw("Speed: %d  ", speed);
+    move(11,1);
+    printw("Led: %d  ", led);
+    move(12,1);
+    printw("Fari: %d  ", fari);
     
     move(14,0);
     printw("--------------------------------------------------------------------------------");
@@ -307,7 +373,11 @@ void printer(){
     printw("d per sterzare a destra");
     move(20,1);
     printw("a per sterzare a sinistra");
-    move(21,0);
+    move(21,1);
+    printw("l per i lampeggianti");
+    move(22,1);
+    printw("f per i fari");
+    move(23,0);
     printw("--------------------------------------------------------------------------------");
     
     move(0,0);
@@ -317,27 +387,62 @@ void printer(){
 }
 
 
+
+
+
+
+
 int main (void)
 {
     initscr();
     
-    int c;
+    int c, pid, status;
     
-    /*if (wiringPiSetup() == -1){
+    if (wiringPiSetup() == -1){
         printw ("ATTENZIONE: wiringPI non inizializzata!\n");
         exit (1);
-    }*/
+    }
     
     speed = 0;
     turn = 0;
+    led = 0;
+    fari = 0;
     
-    /*pinMode (DE, PWM_OUTPUT);
+    dPWM = 0;
+    sPWM = 0;
+    
+    softPwmCreate (DE, 0, 100);
+    softPwmCreate (SE, 0, 100);
+
+
     pinMode (DA, OUTPUT);
     pinMode (DB, OUTPUT);
-    
-    pinMode (SE, PWM_OUTPUT);
     pinMode (SA, OUTPUT);
-    pinMode (SB, OUTPUT);*/
+    pinMode (SB, OUTPUT);
+    
+    pinMode (LED1, OUTPUT);
+    pinMode (LED2, OUTPUT);
+    
+    pinMode (LED3, OUTPUT);
+    pinMode (LED4, OUTPUT);
+    pinMode (LED5, OUTPUT);
+    
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    
+    
+    //inizializzazione gestore LED
+    
+    pid = fork();
+    
+    if (pid == 0){
+        codiceFiglio();
+        exit(0);
+    }
+
     
     move(14,20);
     printw ("Variabili inizializzate correttamente");
@@ -351,31 +456,42 @@ int main (void)
         switch (c) {
             case 'w':
                 accelera();
-                aggiornaPWM(turn, speed);
+                setMotori();
                 printer();
                 break;
                 
             case 's':
                 rallenta();
-                aggiornaPWM(turn, speed);
+                setMotori();
                 printer();
                 break;
                 
             case 'd':
                 giraDx();
-                aggiornaPWM(turn, speed);
+                setMotori();
                 printer();
                 break;
                 
             case 'a':
                 giraSx();
-                aggiornaPWM(turn, speed);
+                setMotori();
                 printer();
                 break;
                 
             case 'b':
                 brake();
-                aggiornaPWM(turn, speed);
+                setMotori();
+                printer();
+                break;
+                
+            case 'l':
+                kill (pid, SIGUSR1);
+                ledswitch();
+                printer();
+                break;
+                
+            case 'f':
+                setFARI();
                 printer();
                 break;
                 
@@ -388,6 +504,10 @@ int main (void)
         
         
     }
+    
+    //termino il gestore LED
+    kill(pid, SIGKILL);
+    wait(&status);
     
     endwin();
     return 0;
